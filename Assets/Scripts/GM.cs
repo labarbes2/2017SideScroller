1edd8bc09e5e961e7b7178f301a265a8a09eaad6
Samantha <labarbes2@hawkmail.newpaltz.edu>
2017-03-30 10:46:58 -0400

    Added Lives and Jump Movements

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -1,16 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GM : MonoBehaviour {
+    public int lives;
+    public int points;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SetLives(int newValue){
+        lives = newValue;
+    }
+
 }
