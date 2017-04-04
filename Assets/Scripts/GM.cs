dcfddd549e6020c586e8056289f88ddd10a40a4a
Samantha <labarbes2@hawkmail.newpaltz.edu>
2017-04-03 23:12:39 -0400

    Added Lives and Reset Position

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GM : MonoBehaviour {
-    public int lives;
+    public int _Lives = 3;
     public int points;
+   
+    public Text livesValue; 
+    public Text pointsValue;
+    
+    public void SetLives(int newValue) {
+        _Lives = newValue;
+        Debug.Log ("Lives now equals: " + _Lives);
+        livesValue.text = _Lives.ToString();
+            } 
 
-    public void SetLives(int newValue){
-        lives = newValue;
+    public int GetLives() {
+        return _Lives;
     }
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,9 +11,11 @@ public class Player : MonoBehaviour {
 
     new Rigidbody2D rigidbody;
     GM _GM;
+    private Vector3 startingPosition;
 
 	// Use this for initialization
-	void Start () { 
+	void Start () {
+        startingPosition = transform.position; 
         rigidbody = GetComponent<Rigidbody2D>();
         _GM = FindObjectOfType<GM>();
 	}
@@ -34,13 +36,16 @@ public class Player : MonoBehaviour {
 
         //Check for out
         if(transform.position.y < deadZone) {
-            Debug.Log("You're Out");
+            Debug.Log("Current Position" + transform.position.y + "is lower than" + deadZone);
+            GetOut();
         }
 
         //rigidbody.AddForce(new Vector2(x * speed, 0)); (could have done this too)
-	}
+	    }
 
     public void GetOut() {
-        _GM.SetLives(_GM.lives - 1);
+        _GM.SetLives( _GM.GetLives() - 1);
+        transform.position = startingPosition;
+        Debug.Log("You're Out");
     }
 }
